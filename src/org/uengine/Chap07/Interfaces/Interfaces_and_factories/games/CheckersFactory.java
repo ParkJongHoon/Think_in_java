009d53ee9306328e354a299fcc816c72ddd61f32
Park <m05214@uengine.org>
2016-10-11 18:46:44 +0900

    2016/10/11 study result

--- src/org/uengine/Chap07/Interfaces/Interfaces_and_factories/games/CheckersFactory.java
+++ src/org/uengine/Chap07/Interfaces/Interfaces_and_factories/games/CheckersFactory.java
@@ -2,7 +2,6 @@ package org.uengine.Chap07.Interfaces.Interfaces_and_factories.games;
 
 public class CheckersFactory implements GameFactory {
 
-	@Override
 	public Game getGame() {return new Chess();}
 	
 }
